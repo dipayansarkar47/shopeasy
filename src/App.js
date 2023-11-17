9aa6a3db8201f917c5b7906f9b2a64c72bcd8314
dipayansarkar47 <dipayansarkar265482@gmail.com>
2023-11-18 00:42:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Home from './components/Home'
-import Features from './components/Features'
+import Home from './pages/Home'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css';
+import ProductDetails from './pages/ProductDetails';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetails></ProductDetails>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Features></Features>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductContextProvider from './contexts/ProductContext';
+import CartContextProvider from './contexts/CartContext';
+import SidebarContextProvider from './contexts/SidebarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SidebarContextProvider>
+
+      <CartContextProvider>
+
+        <ProductContextProvider>
+          <App />
+        </ProductContextProvider>
+      </CartContextProvider>
+    </SidebarContextProvider>
   </React.StrictMode>
 );
 
